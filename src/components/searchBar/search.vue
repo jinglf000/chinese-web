9ac721bae63290965571393994d9d215446ee722
jinglf000 <jinglongfei@jsb5.com>
2018-03-26 14:03:39 +0800

    fix(page): 修复了chrome对于 linear-gradient的兼容性问题

--- src/components/searchBar/search.vue
+++ src/components/searchBar/search.vue
@@ -63,6 +63,13 @@ export default {
   display: flex;
   background-color: $color-input-bg;
   font-size: $font-size-base;
+  background: -webkit-gradient(
+    linear,
+    right top,
+    left top,
+    from(#40e0d0),
+    to(#fff)
+  );
   background: linear-gradient(right, #40e0d0, #fff);
   .input {
     flex: 1 1 80%;
